2e7ab5e141444ae1ebb2e78980bf48c2bf8b5e26
msalmasi <michael@max2.com>
2025-05-29 15:57:14 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3354,7 +3354,7 @@ app.get("/events", (req, res) => {
   res.setHeader("Content-Type", "text/event-stream");
   res.setHeader("Cache-Control", "no-cache");
   res.setHeader("Connection", "keep-alive");
-
+  res.setHeader('X-Accel-Buffering', 'no');
   res.flushHeaders(); // Flush the headers to establish SSE connection
 
   // Register this connection to receive updates for the specified spinId
